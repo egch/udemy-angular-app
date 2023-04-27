f4e74592c87fcfd4c854212c8130a51159c83529
Enrico Giurin <enricogiurin@gmail.com>
2023-04-27 23:02:12 +0200

    Two-Way binding with ngModule

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
-  serverName = '';
+  serverName = 'TestServer';
   serverCreationStatus = 'No server was created!';
 
   constructor(){
